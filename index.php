aa05d0e29e5a9bc62beec6ef286b90558e1f2722
sandeep-tu <stulachan17@gmail.com>
2018-07-24 09:53:26 -0700

    Updated CSS

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ _______________________________&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&
 Woodside Elementary School District&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contractor
 <br><br>
 
-<center><input type="submit" name="submit" value="Create PDF"></center>
+<center><input type="submit" id="myButton" name="submit" value="Create PDF"></center>
 </form>
 
 </div>
